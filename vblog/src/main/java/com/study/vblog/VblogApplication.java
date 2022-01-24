986749a7c4a065f154071e0070752fd21042b88a
duiyi <duiyi@dian.so>
2022-01-24 18:51:28 +0800

    :sparkles: feat: 项目初始化

---
+++ vblog/src/main/java/com/study/vblog/VblogApplication.java
@@ -0,0 +1,13 @@
+package com.study.vblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VblogApplication.class, args);
+    }
+
+}
